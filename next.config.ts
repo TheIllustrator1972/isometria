a1c5879e5d397a9229db0e855f3d12fd61bcb19b
Nilesh K <nileshkamble54321@gmail.com>
2025-01-18 12:48:45 +0530

    - Added Asset Prefix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  assetPrefix: "/isometria",
+  images: {
+    unoptimized: true, // Required for static exports
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
